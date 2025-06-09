06cefd6ece364f8a9c250fa2f05f809885db9274
Tombombadil7 <nirnir1000@gmail.com>
2025-06-09 15:29:12 +0300

    Update Index.tsx
    עריכת טקטס 3

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -121,7 +121,7 @@ const Index = () => {
             <Heart className="w-6 h-6 text-rose-500 fill-current" />
           </div>
           <p className="text-sm text-gray-500" dir="rtl">
-            ספר אורחים אודיו דיגיטלי - מתנת חתונה מיוחדת
+            מהגיס/אח החנון האוהב שלכם, מאחל לכם הרבה שנים של אודר והרפתקאות!!
           </p>
         </div>
       </footer>
